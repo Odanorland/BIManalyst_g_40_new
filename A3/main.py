ae3a1edefabacd408d7a29b9d7def9fa4698cd24
Odanorland <s233197@dtu.dk>
2024-10-28 13:39:57 +0100

    Update main.py

--- A3/main.py
+++ A3/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-model_path = Path("C:\Users\47480\Documents\DTU\41934 Advanced BIM\Week2_Arch.ifc")
+model_path = Path("C:\Users\47480\Documents\DTU\41934 Advanced BIM/A3/CES_BLD_24_11_ARC.ifc")
 if not model_path.is_file():
     raise FileNotFoundError(f"No file found at {model_path}!")
 
