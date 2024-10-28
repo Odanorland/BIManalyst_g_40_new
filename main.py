1d9330f85ab57b35d03bbe0d2edfd704a6f9014a
Odanorland <s233197@dtu.dk>
2024-10-28 13:15:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import ifcopenshell
-
 from .rules import windowRule
 from .rules import doorRule
-
 model = ifcopenshell.open("path/to/ifcfile.ifc")
-
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
-
 print("Window result:", windowResult)
 print("Door result:", doorResult)
+
+
+from pathlib import Path
+
+model_path = Path("C:\Users\47480\Documents\DTU\41934 Advanced BIM\Week2_Arch.ifc")
+if not model_path.is_file():
+    raise FileNotFoundError(f"No file found at {model_path}!")
+
+model = ifcopenshell.open(model_path)
+
+
